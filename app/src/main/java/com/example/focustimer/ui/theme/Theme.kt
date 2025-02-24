af4dd4d5e122b8a983c9e86868df6c4fc65bbed2
JorgeGamez <jorge.gamez.mx@gmail.com>
2025-02-24 17:51:56 -0600

    Initial commit

---
+++ app/src/main/java/com/example/focustimer/ui/theme/Theme.kt
@@ -0,0 +1,65 @@
+package com.example.focustimer.ui.theme
+
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.platform.LocalContext
+
+private val DarkColorScheme = darkColorScheme(
+    primary = primaryWhite,
+    secondary = primaryGray,
+    tertiary = lightGray,
+    surface = primaryBlack,
+    background = primaryBlack
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = primaryBlack,
+    secondary = primaryGray,
+    tertiary = lightGray,
+    surface = primaryWhite,
+    background = primaryWhite
+)
+
+private val LocalDimens = staticCompositionLocalOf { DefaultDimens }
+
+@Composable
+fun ProvideDimens(
+    dimens: Dimens,
+    content: @Composable () -> Unit
+){
+    val dimensionSet = remember { dimens }
+    CompositionLocalProvider(LocalDimens provides dimensionSet, content = content)
+}
+
+@Composable
+fun FocusTimerTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
