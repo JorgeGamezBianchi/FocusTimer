5e5f92606550e354f213c7788b83b12bb9b5aa63
JorgeGamez <jorge.gamez.mx@gmail.com>
2025-02-26 09:15:52 -0600

    Parte 1 del curso

--- app/src/main/java/com/example/focustimer/MainActivity.kt
+++ app/src/main/java/com/example/focustimer/MainActivity.kt
@@ -8,17 +8,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.focustimer.ui.theme.FocusTimerTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            FocusTimerTheme {
+            val windowSize = calculateWindowSizeClass(this)
+            FocusTimerTheme (
+                windowSize = windowSize.widthSizeClass
+            ) {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
--- app/src/main/java/com/example/focustimer/ui/theme/Theme.kt
+++ app/src/main/java/com/example/focustimer/ui/theme/Theme.kt
@@ -7,8 +7,10 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.platform.LocalContext
@@ -45,6 +47,7 @@ fun FocusTimerTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
+    windowSize: WindowWidthSizeClass = WindowWidthSizeClass.Compact,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -57,9 +60,24 @@ fun FocusTimerTheme(
         else -> LightColorScheme
     }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+    val dimensions = if(windowSize > WindowWidthSizeClass.Compact)
+        TabletDimens
+    else
+        DefaultDimens
+
+    ProvideDimens(dimens = dimensions) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content
+        )
+    }
+
+}
+
+object FocusTimerTheme{
+    val dimens: Dimens
+    @Composable
+    @ReadOnlyComposable
+    get() = LocalDimens.current
 }
