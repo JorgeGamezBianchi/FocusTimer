1fcc549891d6717504031d5544c3d02da7f5d399
JorgeGamez <jorge.gamez.mx@gmail.com>
2025-02-26 09:44:26 -0600

    Curso Android Parte 2 - Clean Architecture

--- app/src/main/java/com/example/focustimer/MainActivity.kt
+++ app/src/main/java/com/example/focustimer/MainActivity.kt
@@ -13,7 +13,7 @@ import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.focustimer.ui.theme.FocusTimerTheme
+import com.example.focustimer.presentation.theme.FocusTimerTheme
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
--- app/src/main/java/com/example/focustimer/presentation/theme/Theme.kt
+++ app/src/main/java/com/example/focustimer/presentation/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.example.focustimer.ui.theme
+package com.example.focustimer.presentation.theme
 
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
